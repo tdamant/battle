d1159fccdac58f7cf9e301cd71f6747fcbd07dda
Ross <ross.hunter@tesco.com>
2019-01-23 11:31:15 +0000

    Testing infratsture working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,4 +97,14 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+ENV['RACK_ENV'] = 'test'
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), "..", 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# tell capybara about our app class
+Capybara.app = Battle
 end
