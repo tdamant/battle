8614883f8dbabe6f71704a4e8363c2b25a401b5e
Ross <ross.hunter@tesco.com>
2019-01-23 12:12:52 +0000

    changed to post name to redirect to get play

--- app.rb
+++ app.rb
@@ -8,10 +8,16 @@ enable :sessions
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb :play
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
+
+get '/play' do
+  @player_1_name = session[:player_1_name]
+  @player_2_name = session[:player_2_name]
+  erb :play
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
